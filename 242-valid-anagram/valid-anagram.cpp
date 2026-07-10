5e6d72d0ee372d620b7fe8d17b26e776bc6710ef
harshrajthakurr <harshrajthakurr@gmail.com>
2026-07-10 11:09:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (25.71%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+            return false;
+        vector<int> v1(26,0),v2(26,0);
+        for(int i=0;i<s.size();i++)
+        {
+            v1[s[i]-'a']++;
+            v2[t[i]-'a']++;
+        }
+        if(v1==v2)
+            return true;
+        return false;
+    }
+};
